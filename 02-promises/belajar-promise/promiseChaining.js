4fe30cacdedd466cb44a7b5e3d342486cea7dc89
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-09-18 17:06:16 +0700

    refactoring again promise chaining

--- 02-promises/belajar-promise/promiseChaining.js
+++ 02-promises/belajar-promise/promiseChaining.js
@@ -16,9 +16,9 @@ const arrayKata = ['Hello', 'World', 'Coba', 'Gabung', 'Kata']
 // kita mulai bikin chaining promisenya
 
 gabungKata(arrayKata[0])
-    .then((dataHasilResolvePertama) => gabungKata(dataHasilResolvePertama + " " + arrayKata[1]))
-    .then((dataHasilResolveKedua) => gabungKata(dataHasilResolveKedua + " " + arrayKata[2]))
+    .then((data) => gabungKata(data + " " + arrayKata[1]))
+    .then((data) => gabungKata(data + " " + arrayKata[2]))
     .then((data) => gabungKata(data + " " + arrayKata[3]))
     .then((data) => gabungKata(data + " " + arrayKata[4]))
-    .then((dataHasilSemua) => console.log(dataHasilSemua))
+    .then((data) => console.log(data))
     .catch((errorReject) => console.log(errorReject))
