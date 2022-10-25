f9918fb31407c3c41f34b7c2ce45c95e38bbff69
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-25 16:17:02 +0700

    Day 14 : Throw User Not Auth

--- 14-firebase-authentication/firebase-authentication/src/App.js
+++ 14-firebase-authentication/firebase-authentication/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "./auhentication/firebase";
 
 import NavBar from "./components/NavBar";
 import HomePage from "./containers/HomePage";
 
 function App() {
+  const navigate = useNavigate();
+  const [user, isLoading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, isLoading, navigate]);
+
   return (
     <div className="App">
       <NavBar />
